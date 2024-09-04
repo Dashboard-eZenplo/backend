3c912733a73c525db8f3ed8610f31310e5fe0dd5
Andrei <andreirech22@outlook.com>
2024-09-04 17:47:36 -0300

    feat: adding user route

--- app/main.py
+++ app/main.py
@@ -1,7 +1,8 @@
 from fastapi import FastAPI
 
-from app.routers import csv
+from app.routers import csv, user
 
 app = FastAPI()
 
 app.include_router(csv.router, prefix="/csv", tags=["CSV Operations"])
+app.include_router(user.router, prefix="/register", tags=["Register Operation"])
