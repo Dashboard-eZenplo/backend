2d9c966a19581e9c7f997e914d46e09773790f2d
Andrei <andreirech22@outlook.com>
2024-09-04 20:01:21 -0300

    feat: trying to add the create_user implementation

--- app/services/user_serivce.py
+++ app/services/user_serivce.py
@@ -1,5 +1,6 @@
 from app.schemas.user import UserBase
 from app.utils.cnpj_validator import cnpj_validator
+from app.utils.database import fetch_data, insert_data
 from app.utils.email_validator import email_validator
 from app.utils.phone_validator import phone_validator
 
@@ -19,12 +20,38 @@ async def process_register(user: UserBase):
         if error:
             return {"message": "Error while validating user", "type": error}
 
-    result = await create_user(user)
-    return result
+    return await create_user(user)
 
 
 async def create_user(user: UserBase):
     """
     Creates the user in the database.
     """
-    return {"message": "Succesfully create user", "type": 201}
+    insert_query = """
+    INSERT INTO users (name, cnpj, phone, email, password, admin)
+    VALUES (%s, %s, %s, %s, %s, %s)
+    """
+
+    user_data = (
+        user.name,
+        user.cnpj,
+        user.phone,
+        user.email,
+        user.password,
+        user.admin,
+    )
+
+    success = insert_data(insert_query, user_data)
+
+    if success:
+        return {"message": "User created successfully"}
+    else:
+        return {"message": "Error while creating user"}
+
+
+async def get_all_users():
+    """
+    Get all users from the database.
+    """
+    query = "SELECT * FROM users"
+    return fetch_data(query)
