fadf3413720e8a5aca6b391a1e00f344f453031b
Athosep08 <docsathos@gmail.com>
2024-09-04 18:24:42 -0300

    fix: update user model

--- app/schemas/user.py
+++ app/schemas/user.py
@@ -1,14 +1,14 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 
 class UserBase(BaseModel):
-    name: str
-    cnpj: str
-    phone: str
-    email: str
-    password: str
-    admin: bool | None = False
+    name: str = Field(..., example="John Doe")
+    cnpj: str = Field(..., example="12.345.678/0001-95")
+    phone: str = Field(..., example="55999224455")
+    email: str = Field(..., example="johndoe@example.com")
+    password: str = Field(..., example="strongpassword123")
+    admin: bool | None = Field(default=False, example=False)
 
 
 class User(UserBase):
-    id: int
+    id: int = Field(..., example=1)
