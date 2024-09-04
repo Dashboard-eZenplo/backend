5a1aaaef663e304ae1a2bf83448fb970834dbd54
l-ehara <lucasehara@gmail.com>
2024-09-04 19:13:06 -0300

    Add tables

---
+++ database/11_create_table_ezenplo_city.sql
@@ -0,0 +1,8 @@
+CREATE TABLE ezenplo.city (
+    id INT(10) NOT NULL AUTO_INCREMENT,
+    name VARCHAR(50) NOT NULL,
+    nickname VARCHAR(50) DEFAULT NULL,
+    postal_code VARCHAR(9) DEFAULT NULL,
+    state_code VARCHAR(2) DEFAULT NULL,
+    PRIMARY KEY (id)
+);
