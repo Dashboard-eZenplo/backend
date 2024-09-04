4b15b1ac64e293ff3f583085fff0f462ff6d1bc6
Andrei <andreirech22@outlook.com>
2024-09-04 17:52:25 -0300

    fix: remove import and logic change

--- app/schemas/user.py
+++ app/schemas/user.py
@@ -1,17 +1,14 @@
-from pydantic import BaseModel, EmailStr
+from pydantic import BaseModel
 
 
 class UserBase(BaseModel):
     name: str
     cnpj: str
-    email: EmailStr
     phone: str
+    email: str
+    password: str
     admin: bool | None = False
 
 
 class User(UserBase):
     id: int
-
-
-class UserCreate(UserBase):
-    password: str
