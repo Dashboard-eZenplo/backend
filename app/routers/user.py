6f3eb28084b29f96fc0436a17852e4f9a083cdfe
Nicolas <nicolas.docolas1804@gmail.com>
2024-09-06 15:28:40 -0300

    feat: validate if user already exists in the database Co-authored-by: Andrei Rech <andrei.rech@edu.pucrs.br>

--- app/routers/user.py
+++ app/routers/user.py
@@ -1,7 +1,7 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
 
 from app.schemas.user import UserBase
-from app.services.user_serivce import get_all_users, process_register
+from app.services.user_serivce import *
 
 router = APIRouter()
 
@@ -9,8 +9,11 @@ router = APIRouter()
 @router.post("/register/")
 async def register_user(user: UserBase):
     """
-    Register user route
+    Route to register a user
     """
+    user_db = await get_user_email(user.email)
+    if user_db:
+        raise HTTPException(status_code=400, detail="User already in database")
 
     return await process_register(user)
 
@@ -24,4 +27,19 @@ async def list_users():
     if users:
         return {"users": users}
     else:
-        return {"message": "No users found or error fetching users"}
+        raise HTTPException(status_code=404, detail="No user found")
+
+
+@router.get("/{id}")
+async def get_user(id: int):
+    """
+    Route to get one user in the database by id
+    """
+    try:
+        user = await get_user_id(id)
+        if user:
+            return {"users": user}
+        else:
+            raise HTTPException(status_code=404, detail="No user found")
+    except RuntimeError as e:
+        raise HTTPException(status_code=500, detail=str(e))
