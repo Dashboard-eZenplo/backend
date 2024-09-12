1f43c24f18d44631f1fbdb988091c61af015c3bc
Nicholas Luz <nicholas.s.luz@gmail.com>
2024-09-12 16:05:35 -0300

    change oauth2 to bearer

--- app/services/auth_service.py
+++ app/services/auth_service.py
@@ -1,15 +1,15 @@
-from fastapi import Depends, HTTPException
-from fastapi.security import OAuth2PasswordBearer
+from fastapi import Depends, HTTPException, Security
+from fastapi.security import HTTPAuthorizationCredentials, HTTPBearer
 from jose import JWTError
 from passlib.context import CryptContext
-from starlette.status import HTTP_401_UNAUTHORIZED, HTTP_403_FORBIDDEN
+from starlette.status import HTTP_403_FORBIDDEN
 
 from app.schemas.user import UserBase
 from app.services.jwt_service import extract_all_claims
 from app.services.user_service import get_user_email
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+security = HTTPBearer()
 
 
 def verify_password(plain_password: str, hashed_password: str) -> bool:
@@ -27,23 +27,21 @@ async def authenticate_user(email: str, password: str) -> UserBase:
     return user
 
 
-async def get_current_user(token: str = Depends(oauth2_scheme)):
-    credentials_exception = HTTPException(
-        status_code=HTTP_401_UNAUTHORIZED,
-        detail="Could not validate credentials",
-        headers={"WWW-Authenticate": "Bearer"},
-    )
+async def get_current_user(
+    credentials: HTTPAuthorizationCredentials = Security(security),
+):
+    token = credentials.credentials
 
     try:
         claims = extract_all_claims(token)
         user_id = claims.get("user_id")
         admin = claims.get("admin")
 
-        if user_id is None or admin is None:
-            raise credentials_exception
+        if user_id is None:
+            raise HTTPException(status_code=401, detail="Invalid token")
         return {"user_id": user_id, "admin": admin}
     except JWTError:
-        raise credentials_exception
+        raise HTTPException(status_code=401, detail="Invalid or expired token")
 
 
 async def verify_admin(current_user: dict = Depends(get_current_user)):
